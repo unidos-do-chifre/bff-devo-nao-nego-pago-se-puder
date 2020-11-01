92509350efad94943c4a478c416aff3126580ea7
Yago Gusmão <yagogusmao1998@gmail.com>
2020-10-31 21:24:29 -0300

    init a enviroments variables

--- app.ts
+++ app.ts
@@ -6,6 +6,9 @@ import logger from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
 
+import { config } from 'dotenv';
+config();
+
 import SwaggerSpecs from './src/config/Swagger';
 import swaggerUi from 'swagger-ui-express';
 
@@ -28,28 +31,28 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/*', function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err: any, req: any, res: any, next: any) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-const port = 8080; //|| env.port;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log("App is running on: " + port));
